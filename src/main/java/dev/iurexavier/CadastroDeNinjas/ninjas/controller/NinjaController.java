678af3c30f84f24c558443ac6c530266345e2242
iuxie <iurexavier10@gmail.com>
2026-07-09 00:02:43 -0300

    feat: add initial NinjaController endpoints structure. Closes #6

--- src/main/java/dev/iurexavier/CadastroDeNinjas/ninjas/controller/NinjaController.java
+++ src/main/java/dev/iurexavier/CadastroDeNinjas/ninjas/controller/NinjaController.java
@@ -1,16 +1,44 @@
 package dev.iurexavier.CadastroDeNinjas.ninjas.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")
 public class NinjaController {
 
     @GetMapping("/boasvindas")
     public String boasVindas() {
         return "Hello World!";
+    }
+
+    // Criar Ninja
+    @PostMapping
+    public String criarNinja() {
+        return "Ninja criado!";
+    }
+
+    // Listar todos os Ninjas
+    @GetMapping
+    public String todosOsNinjas() {
+        return "Todos os Ninjas";
+    }
+
+    // Procurar Ninja por ID
+    @GetMapping("/{id}")
+    public String procuraNinjaPorId() {
+        return "Ninja procurado por ID";
+    }
+
+    // Atualizar dados do Ninja
+    @PatchMapping
+    public String atualizaDadosNinja() {
+        return "Dados do Ninja alterados";
+    }
+
+    // Deletar Ninja
+    @DeleteMapping
+    public String deletaNinja() {
+        return "Ninja deletado";
     }
 
 }
