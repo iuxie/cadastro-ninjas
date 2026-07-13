46b836a2474e4783c0e40a425c3c0c137bf0dca0
iuxie <iurexavier10@gmail.com>
2026-07-12 21:40:31 -0300

    feat: add MissaoController mocked routes

--- src/main/java/dev/iurexavier/CadastroDeNinjas/missao/controller/MissaoController.java
+++ src/main/java/dev/iurexavier/CadastroDeNinjas/missao/controller/MissaoController.java
@@ -1,9 +1,34 @@
 package dev.iurexavier.CadastroDeNinjas.missao.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/missoes")
 public class MissaoController {
+
+    // GET - Mandar uma requisicao para buscar missões
+    @GetMapping
+    public String listaMissoes() {
+        return "Missões listadas com sucesso!";
+    }
+
+    // POST - Mandar uma requisicao para criar missões
+    @PostMapping
+    public String criaMissao() {
+        return "Missão criada com sucesso!";
+    }
+
+    // PUT - Mandar uma requisicao para atualizar missões
+    @PutMapping
+    public String atualizaMissao() {
+        return "Missão atualizada com sucesso!";
+    }
+
+    // DELETE - Mandar uma requisicao para deletar missões
+    @DeleteMapping
+    public String deletaMissao() {
+        return "Missão deletada com sucesso!";
+    }
+
+
 }
