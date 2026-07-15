8869734ea22c3f94b75afe7270bd649bde98cff7
iuxie <iurexavier10@gmail.com>
2026-07-14 23:33:13 -0300

    fix: add @Repository annotation to repository interfaces

--- src/main/java/dev/iurexavier/CadastroDeNinjas/missao/repository/MissaoRepository.java
+++ src/main/java/dev/iurexavier/CadastroDeNinjas/missao/repository/MissaoRepository.java
@@ -2,6 +2,8 @@ package dev.iurexavier.CadastroDeNinjas.missao.repository;
 
 import dev.iurexavier.CadastroDeNinjas.missao.service.model.MissaoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MissaoRepository extends JpaRepository<MissaoModel, Long>  {
 }
--- src/main/java/dev/iurexavier/CadastroDeNinjas/ninjas/repository/NinjaRepository.java
+++ src/main/java/dev/iurexavier/CadastroDeNinjas/ninjas/repository/NinjaRepository.java
@@ -2,6 +2,8 @@ package dev.iurexavier.CadastroDeNinjas.ninjas.repository;
 
 import dev.iurexavier.CadastroDeNinjas.ninjas.service.model.NinjaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
 }
