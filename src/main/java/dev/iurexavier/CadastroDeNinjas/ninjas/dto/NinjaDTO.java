d7081d0ec82eaeb908cc70c2535963c139d9c162
iuxie <iurexavier10@gmail.com>
2026-07-20 00:17:00 -0300

    chore: include Ninja DTO and Mapper

--- src/main/java/dev/iurexavier/CadastroDeNinjas/ninjas/dto/NinjaDTO.java
+++ src/main/java/dev/iurexavier/CadastroDeNinjas/ninjas/dto/NinjaDTO.java
@@ -1,4 +1,22 @@
 package dev.iurexavier.CadastroDeNinjas.ninjas.dto;
 
+import dev.iurexavier.CadastroDeNinjas.missao.service.model.MissaoModel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String rank;
+    private MissaoModel missao;
+
 }
