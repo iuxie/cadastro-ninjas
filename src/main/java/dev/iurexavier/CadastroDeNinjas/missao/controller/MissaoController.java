aecdbcca34cd9e609070ee921c2cebd35a489d7c
iuxie <iurexavier10@gmail.com>
2026-07-23 23:13:30 -0300

    feat: implements MissaoDTO in service

--- src/main/java/dev/iurexavier/CadastroDeNinjas/missao/controller/MissaoController.java
+++ src/main/java/dev/iurexavier/CadastroDeNinjas/missao/controller/MissaoController.java
@@ -1,5 +1,6 @@
 package dev.iurexavier.CadastroDeNinjas.missao.controller;
 
+import dev.iurexavier.CadastroDeNinjas.missao.dto.MissaoDTO;
 import dev.iurexavier.CadastroDeNinjas.missao.service.MissaoService;
 import dev.iurexavier.CadastroDeNinjas.missao.service.model.MissaoModel;
 import org.springframework.web.bind.annotation.*;
@@ -18,19 +19,19 @@ public class MissaoController {
 
     // GET - Mandar uma requisicao para buscar missões
     @GetMapping
-    public List<MissaoModel> listarMissoes() {
+    public List<MissaoDTO> listarMissoes() {
         return service.listarMissoes();
     }
 
     // GET - Buscar missões por ID
     @GetMapping("/{id}")
-    public MissaoModel procurarMissaoPorId(@PathVariable Long id) {
+    public MissaoDTO procurarMissaoPorId(@PathVariable Long id) {
         return service.procurarMissaoPorId(id);
     }
 
     // POST - Mandar uma requisicao para criar missões
     @PostMapping
-    public MissaoModel criarMissao(@RequestBody MissaoModel missao) {
+    public MissaoDTO criarMissao(@RequestBody MissaoDTO missao) {
         return service.criarMissao(missao);
     }
 
--- src/main/java/dev/iurexavier/CadastroDeNinjas/missao/service/MissaoService.java
+++ src/main/java/dev/iurexavier/CadastroDeNinjas/missao/service/MissaoService.java
@@ -1,35 +1,46 @@
 package dev.iurexavier.CadastroDeNinjas.missao.service;
 
+import dev.iurexavier.CadastroDeNinjas.missao.dto.MissaoDTO;
+import dev.iurexavier.CadastroDeNinjas.missao.mapper.MissaoMapper;
 import dev.iurexavier.CadastroDeNinjas.missao.repository.MissaoRepository;
 import dev.iurexavier.CadastroDeNinjas.missao.service.model.MissaoModel;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MissaoService {
 
     private final MissaoRepository repository;
+    private final MissaoMapper mapper;
 
-    public MissaoService(MissaoRepository repository) {
+    public MissaoService(MissaoRepository repository, MissaoMapper mapper) {
         this.repository = repository;
+        this.mapper = mapper;
     }
 
     // Listar todas as missões
-    public List<MissaoModel> listarMissoes() {
-        return repository.findAll();
+    public List<MissaoDTO> listarMissoes() {
+        List<MissaoModel> missaoModelList = repository.findAll();
+        return missaoModelList.stream()
+                .map(mapper::map)
+                .toList();
     }
 
     // Buscar Missão por ID
-    public MissaoModel procurarMissaoPorId(Long id) {
+    public MissaoDTO procurarMissaoPorId(Long id) {
         Optional<MissaoModel> missaoModelOptional = repository.findById(id);
-        return missaoModelOptional.orElse(null);
+        return missaoModelOptional.map(mapper::map)
+                .orElse(null);
     }
 
     // Criar Missão
-    public MissaoModel criarMissao(MissaoModel missao) {
-        return repository.save(missao);
+    public MissaoDTO criarMissao(MissaoDTO missao) {
+        MissaoModel missaoModel = mapper.map(missao);
+        missaoModel = repository.save(missaoModel);
+        return mapper.map(missaoModel);
     }
 
     // Deletar Missão
