a72f96f5cf5891f4de30ad29f3eb25d2dc9a795f
iuxie <iurexavier10@gmail.com>
2026-07-26 23:39:31 -0300

    feat: include annotation @ToString on models

--- src/main/java/dev/iurexavier/CadastroDeNinjas/missao/service/model/MissaoModel.java
+++ src/main/java/dev/iurexavier/CadastroDeNinjas/missao/service/model/MissaoModel.java
@@ -13,6 +13,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class MissaoModel {
 
     @Id
--- src/main/java/dev/iurexavier/CadastroDeNinjas/ninjas/service/model/NinjaModel.java
+++ src/main/java/dev/iurexavier/CadastroDeNinjas/ninjas/service/model/NinjaModel.java
@@ -12,6 +12,7 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = "missao")
 public class NinjaModel {
 
     @Id
