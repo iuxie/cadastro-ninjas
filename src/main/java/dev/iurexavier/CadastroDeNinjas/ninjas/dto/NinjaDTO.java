8c3912b606b41c69d34db5b445311f972961427b
iuxie <iurexavier10@gmail.com>
2026-08-01 08:36:44 -0300

    docs: add Swagger annotations in NinjaDTO

--- src/main/java/dev/iurexavier/CadastroDeNinjas/ninjas/dto/NinjaDTO.java
+++ src/main/java/dev/iurexavier/CadastroDeNinjas/ninjas/dto/NinjaDTO.java
@@ -1,6 +1,7 @@
 package dev.iurexavier.CadastroDeNinjas.ninjas.dto;
 
 import dev.iurexavier.CadastroDeNinjas.missao.service.model.MissaoModel;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,11 +13,22 @@ import lombok.Setter;
 @AllArgsConstructor
 public class NinjaDTO {
 
+    @Schema(description = "ID do ninja", example = "1234", hidden = true, requiredMode = Schema.RequiredMode.REQUIRED)
     private Long id;
+
+    @Schema(description = "Nome do Ninja", example = "Naruto Uzumaki", requiredMode = Schema.RequiredMode.REQUIRED)
     private String nome;
+
+    @Schema(description = "E-mail do Ninja", example = "naruto@email.com", requiredMode = Schema.RequiredMode.REQUIRED)
     private String email;
+
+    @Schema(description = "Idade do Ninja", example = "19", requiredMode = Schema.RequiredMode.REQUIRED)
     private int idade;
+
+    @Schema(description = "Rank do Ninja", example = "Kage")
     private String rank;
+
+    @Schema(name = "missao_id", description = "ID da missão", example = "12")
     private MissaoModel missao;
 
 }
