264fbd05142b84ded8e95141264816c354a247d2
iuxie <iurexavier10@gmail.com>
2026-08-03 17:10:25 -0300

    feat: add @Schema documentation in MissaoDTO

--- src/main/java/dev/iurexavier/CadastroDeNinjas/missao/dto/MissaoDTO.java
+++ src/main/java/dev/iurexavier/CadastroDeNinjas/missao/dto/MissaoDTO.java
@@ -1,6 +1,7 @@
 package dev.iurexavier.CadastroDeNinjas.missao.dto;
 
 import dev.iurexavier.CadastroDeNinjas.ninjas.service.model.NinjaModel;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,9 +15,16 @@ import java.util.List;
 @AllArgsConstructor
 public class MissaoDTO {
 
+    @Schema(hidden = true)
     private Long id;
+
+    @Schema(description = "Nome da Missão", example = "Salvar a vila", requiredMode = Schema.RequiredMode.REQUIRED)
     private String nome;
+
+    @Schema(description = "Dificuldade da Missão (S-F)", example = "S", requiredMode = Schema.RequiredMode.REQUIRED)
     private String dificuldade;
+
+    @Schema(description = "Lista de Ninjas responsáveis pela Missão")
     private List<NinjaModel> ninjas;
 
 }
