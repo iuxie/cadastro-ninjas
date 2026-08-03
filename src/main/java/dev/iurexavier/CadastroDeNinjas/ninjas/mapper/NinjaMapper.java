d6359a55f9a2384fcb7f4d9afbcb8265d30b7fd0
iuxie <iurexavier10@gmail.com>
2026-08-03 18:29:52 -0300

    refactor: remove Model attributes from DTOs, anda replace them to DTOs attributes

--- src/main/java/dev/iurexavier/CadastroDeNinjas/ninjas/mapper/NinjaMapper.java
+++ src/main/java/dev/iurexavier/CadastroDeNinjas/ninjas/mapper/NinjaMapper.java
@@ -1,5 +1,7 @@
 package dev.iurexavier.CadastroDeNinjas.ninjas.mapper;
 
+import dev.iurexavier.CadastroDeNinjas.missao.dto.MissaoDTO;
+import dev.iurexavier.CadastroDeNinjas.missao.service.model.MissaoModel;
 import dev.iurexavier.CadastroDeNinjas.ninjas.dto.NinjaDTO;
 import dev.iurexavier.CadastroDeNinjas.ninjas.service.model.NinjaModel;
 import org.springframework.stereotype.Component;
@@ -15,7 +17,15 @@ public class NinjaMapper {
         ninjaModel.setEmail(ninjaDTO.getEmail());
         ninjaModel.setIdade(ninjaDTO.getIdade());
         ninjaModel.setRank(ninjaDTO.getRank());
-        ninjaModel.setMissao(ninjaDTO.getMissao());
+
+        if (ninjaDTO.getMissao() != null) {
+            MissaoModel missaoModel = new MissaoModel();
+            missaoModel.setId(ninjaDTO.getMissao().getId());
+            missaoModel.setNome(ninjaDTO.getMissao().getNome());
+            missaoModel.setDificuldade(ninjaDTO.getMissao().getDificuldade());
+
+            ninjaModel.setMissao(missaoModel);
+        }
 
         return ninjaModel;
     }
@@ -28,9 +38,16 @@ public class NinjaMapper {
         ninjaDTO.setEmail(ninjaModel.getEmail());
         ninjaDTO.setIdade(ninjaModel.getIdade());
         ninjaDTO.setRank(ninjaModel.getRank());
-        ninjaDTO.setMissao(ninjaModel.getMissao());
+
+        if (ninjaModel.getMissao() != null) {
+            MissaoDTO missaoDTO = new MissaoDTO();
+            missaoDTO.setId(ninjaModel.getMissao().getId());
+            missaoDTO.setNome(ninjaModel.getMissao().getNome());
+            missaoDTO.setDificuldade(ninjaModel.getMissao().getDificuldade());
+
+            ninjaDTO.setMissao(missaoDTO);
+        }
 
         return ninjaDTO;
     }
-
 }
